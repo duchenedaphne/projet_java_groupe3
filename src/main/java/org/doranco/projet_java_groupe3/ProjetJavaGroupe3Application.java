2f11e44dbe363069d2b8e551c575e6f1cfef4ca9
duchenedaphne <duchenedaphne@outlook.com>
2022-08-30 09:51:16 +0200

    initial commit

---
+++ src/main/java/org/doranco/projet_java_groupe3/ProjetJavaGroupe3Application.java
@@ -0,0 +1,13 @@
+package org.doranco.projet_java_groupe3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetJavaGroupe3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetJavaGroupe3Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/doranco/projet_java_groupe3/ProjetJavaGroupe3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.doranco.projet_java_groupe3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetJavaGroupe3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
